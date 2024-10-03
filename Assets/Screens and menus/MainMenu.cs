e66657d5fd6ec9b86f9196f288c6b3e5aedc45b1
JoshRobinson2024 <JRobinson2024@elam.co.uk>
2024-10-03 10:44:07 +0100

    more stuff done

--- Assets/Screens and menus/MainMenu.cs
+++ Assets/Screens and menus/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     public void PlayGame() 
     {
         SceneManager.LoadSceneAsync(1);
@@ -17,4 +17,15 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadSceneAsync(2);
     }
 
+    public void Return()
+    { 
+        SceneManager.LoadSceneAsync(0);
+    
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
 }
