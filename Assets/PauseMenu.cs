133efcc7e9405971c64bba582384c79d5f3e2528
JoshRobinson2024 <JRobinson2024@elam.co.uk>
2024-10-04 11:50:27 +0100

    Pause menu set up!

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,56 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        { 
+            if (GameIsPaused)
+            {
+                Resume();
+
+            }
+            else
+            {
+                Pause();
+            }
+        
+        }
+        
+    
+        
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+
+}
+
+
+
+
+
+
+
+
--- Assets/Screens and menus/MainMenu.cs
+++ Assets/Screens and menus/MainMenu.cs
@@ -9,7 +9,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame() 
     {
         SceneManager.LoadSceneAsync(1);
-    
+        Time.timeScale = 1.0f;
     }
 
     public void Credits()
