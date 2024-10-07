1eb5336d1d4dc2ca60d1c6c865e87d46d9190866
JoshRobinson2024 <JRobinson2024@elam.co.uk>
2024-10-07 12:48:05 +0100

    failed collectibles

---
+++ Assets/StarShardCollection.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // check if the object colliding with the coin is the player
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
